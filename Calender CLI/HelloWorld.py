3783945e741a2ae6162869c174ac9d4a946ddcd4
Sai Sasank Vindamuri <imsasankvindamuri@gmail.com>
2024-07-09 21:12:38 +0530

    First Commit

---
+++ Calender CLI/HelloWorld.py
@@ -0,0 +1,9 @@
+print('''
+      
+      Hi! I'm trying to use selenium to make a CLI
+      App that can track important dates and remind
+      you of any of those!
+      
+      Wish me luck!!
+
+''')
